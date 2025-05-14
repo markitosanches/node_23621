05c3a4e94f4fbad929dcad8d148814ceffde5195
Marcos <markitosanches@gmail.com>
2025-05-14 14:44:55 -0400

    Seance 8

--- react/to-do/src/components/Header.js
+++ react/to-do/src/components/Header.js
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
 import Button from './Button'
-
-const Header = ({title = "To Do List"}) => {
-    const onClick = () =>{
-        console.log('Add task')
-    }
+import { useLocation } from 'react-router-dom'
+ 
+const Header = ({title = "To Do List", toggleForm, showAdd}) => {
+    // const onClick = () =>{
+    //     console.log('Add task')
+    // }
+    const location = useLocation()
     return(
         <header className="flex justify-between items-center mb-8">
                 <h1 className="text-2xl"> {title} </h1>
-                <Button text="Add" onClick={onClick} color="btn-blue"/>
+                { location.pathname === '/' && (<Button text={showAdd ? 'Close' : 'Add'} onClick={toggleForm} color={showAdd ? 'btn-red' : 'btn-green'}/>)
+                }
+               
         </header>
     )
 }
