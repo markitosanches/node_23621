01acc17290dc6a4c5773a08431e3916840c11515
Marcos <markitosanches@gmail.com>
2025-06-04 12:58:43 -0400

    Class 12 part 1

--- ecommerce/project-vue/src/views/HomeView.vue
+++ ecommerce/project-vue/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeroHeader/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeroHeader from '@/components/HeroHeader.vue'
 
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
+    HeroHeader
   }
 }
 </script>
